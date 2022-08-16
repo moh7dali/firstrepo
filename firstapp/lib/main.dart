3a8fd1c29fe1c0cf9ed1806869af29d8c49dabd4
unknown <you@example.com>
2022-08-16 21:51:19 +0200

    change tittle

---
+++ firstapp/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(),
+            drawer: Drawer(),
+            body: Container(
+              color: Color(0xFFFF1B6F),
+              child: Text(
+                "hello world from remote",
+                style: TextStyle(fontSize: 50, color: Color(0xFF77FFFA)),
+              ),
+            )));
+  }
+}
